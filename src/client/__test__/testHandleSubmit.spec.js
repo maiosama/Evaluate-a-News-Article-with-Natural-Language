6eba9fbd2a4b6f965861c1344c85d40f96da8f50
maiosama <72415874+maiosama@users.noreply.github.com>
2021-03-24 20:34:37 +0200

    Update testHandleSubmit.spec.js

--- src/client/__test__/testHandleSubmit.spec.js
+++ src/client/__test__/testHandleSubmit.spec.js
@@ -1,6 +1,6 @@
 // to solve ReferenceError: regeneratorRuntime is not defined
 // https://knowledge.udacity.com/questions/174638
-const babelPolyfill = require('babel-polyfill')
+//const babelPolyfill = require('babel-polyfill')
 import {
     handleSubmit
 } from '../js/formHandler'
